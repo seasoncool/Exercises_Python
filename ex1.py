5c897d1a5d43c98c8fd9693d20ac0243a4fdba20
seasoncool <150401848@qq.com>
2014-01-27 22:16:49 +0800

    Update ex1.py

--- ex1.py
+++ ex1.py
@@ -2,8 +2,8 @@
 
 #练习一，用print 配合 冒号打印出字符串
 
-print ”Hello world“
-print ”Hello world again“
+print "Hello world"
+print "Hello world again"
 print "I like typing this."
 print "This is fun."
 print "Yay! Printing."
