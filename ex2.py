2cc97733d6811998d55f913b034e92dbb1586a01
seasoncool <150401848@qq.com>
2014-01-27 22:21:27 +0800

    Create ex2.py

---
+++ ex2.py
@@ -0,0 +1,13 @@
+#Coding:utf-8
+
+#如何使用注释和井号
+
+#A comment, this is so you can read your program later.
+#Anything after the # is ignored by python.
+
+print "I could have code like this." # and the comment after is ignored
+
+#You can also sue a comment to "disable" or comment out a piece of code:
+#print "This won't run."
+
+print "This will run."
