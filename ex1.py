bbaa5123e71631147d115cd9dfcb3a5657d83104
seasoncool <150401848@qq.com>
2014-01-27 22:11:36 +0800

    Create ex1.py

---
+++ ex1.py
@@ -0,0 +1,11 @@
+#Coding:utf-8
+
+#练习一，用print 配合 冒号打印出字符串
+
+print ”Hello world“
+print ”Hello world again“
+print "I like typing this."
+print "This is fun."
+print "Yay! Printing."
+print "I'd much rather you 'not'"
+print "I 'said' do not touch this."
