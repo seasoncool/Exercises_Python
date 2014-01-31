42a7e785e7c3cc997ca3c985c9c9ecf6bca51058
seasoncool <150401848@qq.com>
2014-01-31 18:40:36 +0800

    Create ex12(提示别人).py

---
+++ ex12(提示别人).py
@@ -0,0 +1,10 @@
+#coding:utf-8
+
+#提示别人
+
+age = raw_input("How old are you?")
+height = raw_input("How tall are you?")
+weight = raw_input("How much do you weight?")
+
+print "So, you are %r old, %r tall and %r heavy."%(
+	age,height,weight)
