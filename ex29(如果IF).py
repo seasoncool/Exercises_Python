f4ede6a31dcbfa7515a7eb2aee9a40f71177fc60
seasoncool <150401848@qq.com>
2014-02-02 16:20:53 +0800

    Create ex29(如果IF).py

---
+++ ex29(如果IF).py
@@ -0,0 +1,26 @@
+#coding:utf-8
+
+#如果(if)
+
+people = 20
+cats = 30
+dogs = 15
+
+if people < cats:
+	print "Too many cats! The world is doomed!"
+if people > cats:
+	print "Not many cats! The world is saved!"
+if people < dogs:
+	print "The world is drooled on!"
+if people > dogs:
+	print "The world is dry!"
+
+dogs = dogs + 5
+
+if people > dogs:
+	print "people are greater than or equal to dogs."
+if people < dogs:
+	print "people are less than or equal to dogs."
+
+if people == dogs:
+	print "People are dogs"
