3a7e1684953225720c159684891171c967c0eb0b
seasoncool <150401848@qq.com>
2014-02-03 22:41:56 +0800

    Create ex38(列表的操作).py

---
+++ ex38(列表的操作).py
@@ -0,0 +1,64 @@
+#coding:utf-8
+
+#列表的操作 38
+
+
+#create a mapping of state to abbreviation
+
+states = {
+	'Oregon':'OR',
+	'Florida':'FL',
+	'California':'CA',
+	'New York':'NY',
+	'Michigan':'MI'
+}
+
+
+#create a basic set of states and some cities in them
+
+cities = {
+	'CA':'San Francisco',
+	'MI':'Detroit',
+	'FL':'Jacksonville'
+	}
+
+#add some more cities
+cities['NY'] = 'New York'
+cities['OR'] = 'Portland'
+
+#print some states
+
+print '-'*10
+print "Michigan's abbreviation is :",states['Michigan']
+print "Florida's abbreviation is :",states['Florida']
+
+#do it by using the state then cities dict
+print '-'*10
+for state,abbrev in states.items():
+	print "%s is abbreviated %s" %(state,abbrev)
+
+#print every city in state
+print '-'*10
+
+for abbrev,city in cities.items():
+	print "%s has the city %s" %(abbrev,city)
+
+#now do both at the same time
+print '-'*10
+
+for state,abbrev in states.items():
+	print "%s state is abbreviated %s and has city %s"%(
+		state,abbrev,cities[abbrev])
+
+print '-'*10
+#safely get a abbreviation by state that might not be there
+
+state = states.get('Texas',None)
+
+if not state:
+	print "Sorry,no Texas."
+
+#get a city with a default value
+
+city = cities.get('TX','Does Not Exist')
+print "The city for the state 'Tx' is %s" %city
